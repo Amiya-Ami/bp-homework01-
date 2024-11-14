f2f716d0aff78af8b53fd2e3dce4eae480000783
AmiYah <hp1000ssr@gmail.com>
2024-11-15 02:44:45 +0700

    add; index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+
+//  1.
+const Arr101 = [1,2,3];
+const Arr102 = [4,5,6];
+
+const Arr103 = Arr101.concat(Arr102);
+
+console.log(Arr103);
+
+
+//   2.
+
+const Arr201 = [10,20,30,40,50];
+
+const Arr202 =  Arr201.filter((value) => value > 30 );
+
+console.log(Arr202);
+
+
+//   3. 
+
+const Arr301 = [5,6,7,2,4];
+
+Arr301[2] = Arr301[4];
+Arr301.pop();
+
+console.log(Arr301);
+
+
+//   4.
+
+let Num41 = "XIV" ;
+
+function Num042(value,index)  {
+
+
+
+}
+
+
+console.log(Num41);
+
+
+//   5.
+
+const Arr501 = [1,2,3,4,5,2,3,4,1,1,4];
+
+
+
+
+
+//    6. 
